45671c0859e7387cdf72485f5e89d71c7169bb00
Yousef Al Zeer <iyousefzeer@gmail.com>
2025-05-18 15:50:54 +0300

    UPD user-scripts

--- User-Scripts.sh
+++ User-Scripts.sh
@@ -32,3 +32,6 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 echo " This is an app server in AWS Region US-EAST-1 " > /var/www/html/index.ht
+
+
+######################## THE END ########################
